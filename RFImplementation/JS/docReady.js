41f1a52fd53c5b69a64935a9e10633b683226bdd
LEP5KOR <NagaSaiNarayanaKartheek.Palepu@in.bosch.com>
2019-12-27 23:00:01 +0530

    Deleted many files. Added mylocations.html

--- RFImplementation/JS/docReady.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('input[type="submit"]').attr('disabled', 'disabled');
-	$("svg").mouseup(function(){
-		if(arr.length > 0) {
-			$('input[type="submit"]').removeAttr('disabled');
-		}
-	});
-});
