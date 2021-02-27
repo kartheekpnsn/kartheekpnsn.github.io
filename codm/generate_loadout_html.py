584af01558b244aa946e0e5a37ffcb98e67e1128
kartheek <kartheek.palepu@in.bosch.com>
2021-02-27 11:32:51 +0530

    Re-ordered columns

--- codm/generate_loadout_html.py
+++ codm/generate_loadout_html.py
@@ -6,24 +6,25 @@ def generate_tr_text(text):
     else:
         return f"\t<td style='text-align: center; vertical-align: middle;'>{text}</td>\n"
 
-loadouts_data = pd.read_excel("../codm_loadouts.xlsx")
+loadouts_data = pd.read_excel("codm_loadouts.xlsx")
 loadouts_data = loadouts_data.fillna("")
 
-with open("../loadouts_template.html") as fb:
+with open("loadouts_template.html") as fb:
     loadouts_template = fb.read()
 
 loadout_text = ""
 for idx, row in loadouts_data.iterrows():
     loadout_text = loadout_text + f"<tr>\n"
     loadout_text = loadout_text + f"\t<td style='text-align: center; vertical-align: middle;'><b>{row['Gun']}</b></td>\n"
-    loadout_text = loadout_text + generate_tr_text(row['Mode'])
-    loadout_text = loadout_text + generate_tr_text(row['From'])
     _tmp_text = "<ul>\n"
     for col in list(loadouts_data.columns):
         if col not in ['From', 'Mode', 'Gun'] and row[col] != "":
             _tmp_text = _tmp_text + f"<li><b>{col}</b>: {row[col]}</li>"
-    loadout_text = loadout_text + f"<td>{_tmp_text}</td>"
+    loadout_text = loadout_text + f"<td>{_tmp_text}</td>\n"
+    loadout_text = loadout_text + generate_tr_text(row['Mode'])
+    loadout_text = loadout_text + generate_tr_text(row['From'])
+    loadout_text = loadout_text + f"</tr>\n"
 
 loadouts_template = loadouts_template.replace("<tr_replace>", loadout_text)
-with open("../loadouts.html", "w") as fb:
+with open("loadouts.html", "w") as fb:
     fb.write(loadouts_template)
